4a313cbf47bf8ff82d0f8e218d4b0edccd2970ea
meso272 <liujinyang1201@126.com>
2020-11-04 17:32:23 -0800

    lab3

---
+++ sieve3.c
@@ -0,0 +1,86 @@
+/*
+ *   Sieve of Eratosthenes
+ *
+ *   Programmed by Michael J. Quinn
+ *
+ *   Last modification: 7 September 2001
+ */
+
+#include "mpi.h"
+#include <math.h>
+#include <stdio.h>
+#include <stdlib.h>
+#define MIN(a,b)  ((a)<(b)?(a):(b))
+
+int main (int argc, char *argv[])
+{
+   unsigned long int    count;        /* Local prime count */
+   double elapsed_time; /* Parallel execution time */
+   unsigned long int    first;        /* Index of first multiple */
+   int   local_first;
+   unsigned long int    global_count = 0; /* Global prime count */
+   unsigned long long int    high_value;   /* Highest value on this proc */
+   unsigned long int    i;
+   int    id;           /* Process ID number */
+   unsigned long int    index;        /* Index of current prime */
+   unsigned long long int    low_value;    /* Lowest value on this proc */
+   char  *marked;       /* Portion of 2,...,'n' */
+   char  *local_prime_marked;
+   unsigned long long int    n;            /* Sieving from 2, ..., 'n' */
+   int    p;            /* Number of processes */
+   unsigned long int    proc0_size;   /* Size of proc 0's subarray */
+   unsigned long int    prime;
+   unsigned long int  local_prime;        /* Current prime */
+   unsigned long int    size;         /* Elements in 'marked' */
+   unsigned long int  local_prime_size;
+
+
+   MPI_Init (&argc, &argv);
+
+   /* Start the timer */
+
+   MPI_Comm_rank (MPI_COMM_WORLD, &id);
+   MPI_Comm_size (MPI_COMM_WORLD, &p);
+   MPI_Barrier(MPI_COMM_WORLD);
+   elapsed_time = -MPI_Wtime();
+
+   if (argc != 2) {
+      if (!id) printf ("Command line: %s <m>\n", argv[0]);
+      MPI_Finalize();
+      exit (1);
+   }
+
+   n = atoll(argv[1]);
+
+   /* Figure out this process's share of the array, as
+      well as the integers represented by the first and
+      last array elements */
+
+   /* Stop the timer */
+
+   elapsed_time += MPI_Wtime();
+
+   /* Add you code here  */
+
+
+
+
+
+
+
+
+
+
+
+
+
+   /* Print the results */
+
+   if (!id) {
+      printf("The total number of prime: %ld, total time: %10.6f, total node %d\n", global_count, elapsed_time, p);
+
+   }
+   MPI_Finalize ();
+   return 0;
+}
+
